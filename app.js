71e6821218a1befb0152edd68881d8df7a88506e
rawatmanoj <42403468+rawatmanoj@users.noreply.github.com>
2020-02-13 07:32:27 +0530

    adding

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-// const navSlide=(()=>{
+const navSlide=(()=>{
 
-// const burger=document.querySelector('.burger');
-// const nav=document.querySelector('.nav-links');
-// const navlinks= document.querySelectorAll('.nav-links li');
+const burger=document.querySelector('.burger');
+const nav=document.querySelector('.nav_links');
+const navlinks= document.querySelectorAll('.nav_links li');
 
-// burger.addEventListener('click',()=>{
+burger.addEventListener('click',()=>{
 
-// nav.classList.toggle('nav-active');
+nav.classList.toggle('nav-active');
 
 
 
-// navlinks.forEach((link,index)=>{
+navlinks.forEach((link,index)=>{
 
-//     if(link.style.animation){
-//         link.style.animation='';
-//     }else{
+    if(link.style.animation){
+        link.style.animation='';
+    }else{
 
-//     link.style.animation=`navLinkFade 0.5s ease forwards ${index/7+0.5}s`;
+    link.style.animation=`navLinkFade 0.5s ease forwards ${index/7+0.5}s`;
     
-//     }
-//     });
+    }
+    });
 
-//     burger.classList.toggle('toggle');
+    burger.classList.toggle('toggle');
     
 
-// });
+});
 
 
-// })();
+})();
 
 
 
